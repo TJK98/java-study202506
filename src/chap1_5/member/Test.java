990ac0ca3731f2a990286d18b4a64713f153185a
TJK98 <wtj1998@naver.com>
2025-07-02 10:19:03 +0900

    JAVA-chap1_5-member-Test

--- src/chap1_5/member/Test.java
+++ src/chap1_5/member/Test.java
@@ -5,26 +5,27 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
 
-        /*Member m1 = new Member("1234abc!", "TJK", "tjk98@gmail.com", Gender.MALE, 10);
-        System.out.println("m1 = " + m1);*/
+//        Member m1 = new Member(30, "abc1234@gmail.com", "1234", "말포이", Gender.MALE);
+//        System.out.println(m1);
 
-        // 클래스 안에 있는 데이터는 반드시 객체 생성을 통해 접근함
+        // 클래스 안에 있는 데이터는 반드시 객체생성을 통해 접근함.
         MemberRepository mr = new MemberRepository();
 
-        // 맴버 추가 테스트
-        mr.addMember(new Member("0807", "wang", "wang@gmail.com",Gender.MALE, 27));
+        // 멤버 추가 테스트
+        mr.addMember(new Member(80, "oldman@gmail.com", "9888", "하라버지", Gender.MALE));
 
         System.out.println(Arrays.toString(mr.getMembers()));
         System.out.println(mr.size());
 
-        System.out.println("=========================================");
+        System.out.println("================");
 
-        Member foundMember = mr.findMemberByEmail("wang@gmail.com");
+        Member foundMember = mr.findMemberByEmail("oldman@gmail.com");
         System.out.println("foundMember = " + foundMember);
 
-        System.out.println("=========================================");
-        boolean flag = mr.isDuplicateEamil("choi@naver.com");
+        System.out.println("===============");
+        boolean flag = mr.isDuplicateEmail("oldman123@gmail.com");
         System.out.println("flag = " + flag);
+
 
     }
 }
