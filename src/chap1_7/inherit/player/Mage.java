fb49cad66956f75a038487b8adebe229a0b73fd8
TJK98 <wtj1998@naver.com>
2025-07-02 10:22:16 +0900

    JAVA-chap1_7-inherit-Add-Mage, Main, Example

---
+++ src/chap1_7/inherit/player/Mage.java
@@ -0,0 +1,21 @@
+package chap1_7.inherit.player;
+
+public class Mage extends Player {
+
+    int mana; // 마력게이지
+
+    public Mage(String nickName) {
+        super(nickName, 40);
+        this.mana = 100;
+    }
+
+    @Override
+    public void showStatus() {
+        super.showStatus();
+        System.out.println("# 마력: " + this.mana);
+    }
+
+    void fireBall() {
+        System.out.printf("%s님이 파이어볼을 시전합니다.\n", this.nickName);
+    }
+}
