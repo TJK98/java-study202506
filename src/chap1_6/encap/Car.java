c41c5706b8ffeb4dd7ca69f8efa8d459a290c661
TJK98 <wtj1998@naver.com>
2025-07-02 10:20:32 +0900

    JAVA-chap1_6-encap-Refactor-Car, Main

--- src/chap1_6/encap/Car.java
+++ src/chap1_6/encap/Car.java
@@ -1,19 +1,25 @@
 package chap1_6.encap;
 
+
 public class Car {
 
     private String model; // 모델명
-    private String mode; // 변속 모드 (P, N, R, D)
+    private String mode; // 변속모드 (P, N, R, D)
     private String ownerName; // 차주명
     private boolean on; // 시동 여부
 
-    // 변속을 수행하는 메서드
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    // 변속을 수행하는 메서드 (setter 메서드: 필드값을 안전하게 처리하는 메서드)
     public void setMode(String mode) {
         switch (mode) {
-            case "D":
-            case "R":
-            case "N":
-            case "p":
+            case "D": case "R": case "N": case "P":
                 this.mode = mode;
                 break;
             default:
@@ -21,8 +27,12 @@ public class Car {
         }
     }
 
+    public String getMode() {
+        return mode;
+    }
+
     // 기능
-    // 엔진 오일을 분산 하는 기능
+    // 엔진오일을 분사하는 기능
     private void injectOil() {
         System.out.println("엔진오일이 분사됩니다.");
     }
@@ -30,7 +40,7 @@ public class Car {
     // 엔진 실린더가 반복 작동하는 기능
     private void moveCylinder() {
         if (!on) {
-            System.out.println("시동이 안 걸린 상태에서는 차량이 폭발합니다.");
+            System.out.println("시동이 안걸린 상태에서는 차량이 폭발합니다 뻥!");
             return;
         }
         System.out.println("엔진 실린더가 작동합니다.");
@@ -43,10 +53,30 @@ public class Car {
     }
 
     // 시동 버튼을 누르는 기능
-    void pressButton() {
+    public void pressButton() {
         System.out.println("시동 버튼을 눌렀습니다.");
         injectOil();
         powerOn();
         moveCylinder();
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
     }
 }
--- src/chap1_6/encap/Main.java
+++ src/chap1_6/encap/Main.java
@@ -1,16 +1,18 @@
 package chap1_6.encap;
 
 public class Main {
+
     public static void main(String[] args) {
 
-
-        Car myCar = new Car();
-        // 시동을 걸기 위해서 먼저 엔진 오일을 분사
-        // 실린더 작동 -> 시동 버튼 누르고 -> 시동 걸기
+        Car myCar = new Car("모닝");
+        // 시동 걸기 위해서 먼저 엔진오일 분사 ->
+        //  시동버튼 누르고 -> 시동 걸기 -> 실린더 작동
         myCar.pressButton();
 
+//        myCar.model = "페라리488 스파이더";
         // 기어 변속
-        myCar.mode = "D";
-        System.out.println("현재 모드:" + myCar.mode);
+        myCar.setMode("XYZ");
+        System.out.println("현재 모드: " + myCar.getMode());
+        System.out.println("모델명: " + myCar.getModel());
     }
 }
