6d09b8e9708852245e2895640e58e15b466a3da7
TJK98 <wtj1998@naver.com>
2025-07-08 15:45:15 +0900

    JAVA-chap2_7-lambda-Add-Pet-functional-interface,-Cat-class,-and-Main-to-demonstrate-lambda-usage

---
+++ src/chap2_7/lambda/basic/Main.java
@@ -0,0 +1,40 @@
+package chap2_7.lambda.basic;
+
+public class Main {
+
+    public static void order(Pet pet) {
+        System.out.println("주인님의 명령입니다!");
+        // 콜백 함수 처리
+        pet.eat();
+        System.out.println("꺼어어억!");
+    }
+
+    public static void main(String[] args) {
+
+        /*Pet dog = new Pet() {
+            @Override
+            public void eat() {
+                System.out.println("강아지는 사료를 먹어요~");
+            }
+        };*/
+
+        Pet dog = () -> System.out.println("강아지는 사료를 먹어요~");
+        order(dog);
+
+        System.out.println("============");
+
+        order(new Cat());
+
+        System.out.println("============");
+
+        order(() -> System.out.println("호랑이는 생고기를 먹어요 크앙!"));
+        System.out.println("============");
+
+        order(() -> {
+            System.out.println("하하호호");
+            System.out.println("메롱메롱");
+        });
+
+
+    }
+}
